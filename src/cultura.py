40f999650ea282c67e1b1f619ca1b4edfb40cdfb
Leandro <lando.augusto20@gmail.com>
2025-03-24 14:29:46 -0300

    ajustes

--- src/cultura.py
+++ src/cultura.py
@@ -71,13 +71,13 @@ class Cultura():
 
         novo_produto = Produto(codigo_cultura, nome_produto, quantidade_produto_int, comprimento_int, rua_int)  
         print("Produto Cadastrado !!!")
-        print(f'{'cultura'.ljust(25)} | {'codigo do produto'.ljust(25)} | {'nome do produto'.ljust(25)} | {'quantidade  de produto (mL)'.ljust(25)} | {'comprimento'.ljust(25)} | {'largura rua '.ljust(25)}')
+        print(f'{'cultura'.ljust(25)} | {'codigo do produto'.ljust(25)} | {'nome do produto'.ljust(25)} | {'quantidade  de produto (mL)'.ljust(25)} | {'largura rua '.ljust(25)} | {'comprimento'.ljust(25)}')
         cultura_str = 'Cafe' if codigo_cultura == '1' else 'Cana de açucar '
         codigo_str = str(novo_produto._codigo)             
         quantidade_str = str(novo_produto._quantidade) 
         comprimento_str = str(novo_produto._comprimento)           
         rua_str = str(novo_produto._rua)           
-        print(f'{cultura_str.ljust(25)} | {codigo_str.ljust(25)} | {novo_produto._nome_produto.ljust(25)} | {quantidade_str.ljust(25)} | {comprimento_str.ljust(25)} | {rua_str.ljust(25) } ')     
+        print(f'{cultura_str.ljust(25)} | {codigo_str.ljust(25)} | {novo_produto._nome_produto.ljust(25)} | {quantidade_str.ljust(25)}  | {rua_str.ljust(25) }  | {comprimento_str.ljust(25)}')     
         print("\n")
 
     def atualizar_produto():        
@@ -96,7 +96,7 @@ class Cultura():
                 quantidade_produto_int = float(quantidade_produto)
                 break
             else:
-                print("Codigo produto invalído")
+                print("Codigo produto invalído") 
 
         while(True):
             comprimento =  input("Digite o comprimento do lado do quadrado: ") 
--- src/produto.py
+++ src/produto.py
@@ -15,14 +15,14 @@ class Produto:
         return f'{self._codigo} | {self._cultura} | {self._nome_produto} | {self._quantidade} | {self._comprimento} | {self._rua}'
 
     def listar_produto():        
-        print(f'{'cultura'.ljust(25)} | {'codigo do produto'.ljust(25)} | {'nome do produto'.ljust(25)} | {'quantidade de produto (mL)'.ljust(35)} | {'comprimento'.ljust(25)} | {'largura rua '.ljust(25)} ')
+        print(f'{'cultura'.ljust(25)} | {'codigo do produto'.ljust(25)} | {'nome do produto'.ljust(25)} | {'quantidade de produto (mL)'.ljust(35)}  | {'largura rua '.ljust(25)} | {'comprimento'.ljust(25)} ')
         for item in Produto.produtos:            
                 cultura_str = 'Cafe' if item._cultura == '1' else 'Cana de açucar'
                 codigo_str = str(item._codigo)             
                 quantidade_str = str(item._quantidade )        
                 comprimento_str = str(item._comprimento )           
                 rua_str = str(item._rua )           
-                print(f'{cultura_str.ljust(25)} | {codigo_str.ljust(25)} | {item._nome_produto.ljust(25)} | {quantidade_str.ljust(35) } | {comprimento_str.ljust(25) } | {rua_str.ljust(25) }')
+                print(f'{cultura_str.ljust(25)} | {codigo_str.ljust(25)} | {item._nome_produto.ljust(25)} | {quantidade_str.ljust(35) }  | {rua_str.ljust(25) }  | {comprimento_str.ljust(25)}')     
 
     def atualizar_produto(codigo_produto, nome_produto, quantidade_produto, comprimento, rua):         
         for item in Produto.produtos:   
